9dd7bec44124eb780c6701f95a1b1ed1e1e95c4b
Uno Kim <djkehh@gmail.com>
2022-02-17 05:59:43 +0900

    Add #5 - base path of ArticleController
    게시글 컨트롤러는 모두 `/articles`로 경로가 시작하게끔 해서
    게시글 엔드포인트 url 에 통일성을 주고 반복을 줄이도록 유도

--- src/main/java/com/fastcampus/boardpractice/controller/ArticleController.java
+++ src/main/java/com/fastcampus/boardpractice/controller/ArticleController.java
@@ -2,16 +2,18 @@ package com.fastcampus.boardpractice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
-    @GetMapping("/articles")
+    @GetMapping
     public ModelAndView articles() {
         Map<String, Object> map = new HashMap<>();
         map.put("articles", List.of("article")); // TODO: 게시글 목록 넣어주기
