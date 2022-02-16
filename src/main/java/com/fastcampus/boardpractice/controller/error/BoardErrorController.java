88aa7bbe20a912d1e57e430d72052471246dc24c
Uno Kim <djkehh@gmail.com>
2022-02-17 06:11:50 +0900

    Add #5 - global error controller using @ControllerAdvice
    `@ControllerAdvice`를 사용해
    전체 컨트롤러에서 발생하는 에러를 관리하는
    에러 처리 전용 컨트롤러 만들기.
    서비스 로직에서 발생할 구체적인 에러 계획이 없으므로
    별다른 에러 처리 핸들러 메소드는 없음
    `ResponseEntityExceptionHandler`를 이용해
    부트가 정한 기본 REST API 에러 처리를 추가

---
+++ src/main/java/com/fastcampus/boardpractice/controller/error/BoardErrorController.java
@@ -0,0 +1,8 @@
+package com.fastcampus.boardpractice.controller.error;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class BoardErrorController extends ResponseEntityExceptionHandler {
+}
