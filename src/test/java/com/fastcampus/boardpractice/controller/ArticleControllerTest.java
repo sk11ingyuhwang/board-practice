af880b3117204e06ca961cb0925f01de82e76334
Uno Kim <djkehh@gmail.com>
2022-02-16 11:27:43 +0900

    Implement #5 - ArticleController
    게시글 컨트롤러 구현 시작
    일단 기본 컨트롤러 한 개만 구현, 테스트
    게시글 뷰가 보여줄 `index.html` 추가, 내용은 아직 없음

---
+++ src/test/java/com/fastcampus/boardpractice/controller/ArticleControllerTest.java
@@ -0,0 +1,39 @@
+package com.fastcampus.boardpractice.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    @DisplayName("[GET] /articles - 검색어가 없으면, 페이징 목록을 조회한다")
+    @Test
+    void givenNoParameter_whenReadingArticles_thenReturnsArticlePages() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(
+                get("/articles")
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+}
